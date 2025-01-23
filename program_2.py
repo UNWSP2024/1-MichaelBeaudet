e5bdd774e9e61fd344b0ce0e2c7ca853d9cb0436
MichaelBeaudet <michaeltbeaudetalt@gmail.com>
2025-01-22 22:27:45 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,6 +1,9 @@
+#Author: Michael Beaudet
+#Title Modified Hello Part Two
+#Date 1/22/25
 def proverbs_22_29():
-    # Write your Proverbs 22:29 code below
-    print('...')
+
+    print('Seest thou a man diligent in his business? he shall stand before kings; he shall not stand before mean men.')
     
 
 proverbs_22_29()
